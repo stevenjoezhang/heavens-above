30dc26970e5e93feaec49a02e206e351ecd5828f
Mimi <1119186082@qq.com>
2020-02-02 15:08:59 +0800

    Rename `count` -> `pages`

--- run.js
+++ run.js
@@ -9,12 +9,12 @@ var location = [39.9042, 116.4074, "%E5%8C%97%E4%BA%AC%E5%B8%82", 52, "ChST"];
 
 satellite.getTable({
 	target: 25544,
-	count: 4,
+	pages: 1,
 	root: "./public/data/"
 }); //ISS
 /*
 iridium.getTable({
-	count: 4,
+	pages: 4,
 	root: "./public/data/"
 });
 */
