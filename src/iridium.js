973d05e04ce0f2031bdd22a4048ecfa122256cfd
Mimi <1119186082@qq.com>
2020-02-02 15:20:28 +0800

    Drop async

--- src/iridium.js
+++ src/iridium.js
@@ -1,8 +1,7 @@
 const request = require("request");
 const cheerio = require("cheerio");
-const async = require("async");
 const fs = require("fs");
-const base = require("./base");
+const utils = require("./utils");
 
 const eventsIridium = ["brightness", "altitude", "azimuth", "satellite", "distanceToFlareCentre", "brightnessAtFlareCentre", "date", "time", "distanceToSatellite", "AngleOffFlareCentre-line", "flareProducingAntenna", "sunAltitude", "angularSeparationFromSun", "image", "id"];
 
@@ -12,91 +11,95 @@ function getTable(config) {
 	var opt = config.opt || 0;
 	var basedir = config.root + "IridiumFlares/";
 	if (counter === 0) {
-		options = base.get_options("IridiumFlares.aspx?");
+		options = utils.get_options("IridiumFlares.aspx?");
 		if (!fs.existsSync(basedir)) {
 			fs.mkdir(basedir, (err) => {
 				if (err) console.log(err);
 			});
 		}
 	} else {
-		options = base.post_options("IridiumFlares.aspx?", opt);
+		options = utils.post_options("IridiumFlares.aspx?", opt);
 	}
-	request(options, (error, response, body) => { //请求成功的处理逻辑
-		if (!error && response.statusCode === 200) {
-			var $ = cheerio.load(body, {
-				decodeEntities: false
-			});
-			var next = "__EVENTTARGET=&__EVENTARGUMENT=&__LASTFOCUS=";
-			var tbody = $("form").find("table.standardTable tbody");
-			var queue = [];
-			tbody.find("tr").each((i, o) => {
-				var temp = {};
-				for (var i = 0; i < 6; i++) {
-					temp[eventsIridium[i]] = $(o).find("td").eq(i + 1).text();
-				}
-				temp["url"] = "https://www.heavens-above.com/" + $(o).find("td").eq(0).find("a").attr("href").replace("type=V", "type=A");
-				queue.push(temp);
-			});
-			async.map(queue, function(temp, finish) {
-				request(base.iridium_options(temp["url"]), (error, response, body) => {
-					//console.log(response.statusCode)
-					if (!error && response.statusCode === 200) { //在无SessionID时返回500
-						var $ = cheerio.load(body, {
-							decodeEntities: false
-						});
-						var table = $("form").find("table.standardTable"),
-							tr = table.find("tbody tr");
-						[
-							[6, 0],
-							[7, 1],
-							[8, 6],
-							[9, 7],
-							[10, 9],
-							[11, 10],
-							[12, 11]
-						].forEach((ele) => {
-							temp[eventsIridium[ele[0]]] = tr.eq(ele[1]).find("td").eq(1).text();
-						});
-						temp[eventsIridium[13]] = "https://www.heavens-above.com/" + $("#ctl00_cph1_imgSkyChart").attr("src") //.replace("size=800", "size=1600");,
-						var id = base.md5(Math.random().toString()) //temp[eventsIridium[6]];
-						temp[eventsIridium[14]] = id;
-						fs.appendFile(basedir + id + ".html", table.html(), (err) => {
-							if (err) console.log(err);
-						}); //保存表格
-						request.get(base.image_options(temp[eventsIridium[13]])).pipe(fs.createWriteStream(basedir + id + ".png", {
-							"flags": "a"
-						})).on("error", (err) => {
-							console.error(err);
-						}); //下载图片
-						//console.log(temp);
-						finish(null, temp);
+	request(options, (error, response, body) => {
+		console.log(body)
+		if (error || response.statusCode !== 200) return;
+		var $ = cheerio.load(body, {
+			decodeEntities: false
+		});
+		var next = "__EVENTTARGET=&__EVENTARGUMENT=&__LASTFOCUS=";
+		var tbody = $("form").find("table.standardTable tbody");
+		var queue = [];
+		tbody.find("tr").each((i, o) => {
+			var temp = {};
+			for (var i = 0; i < 6; i++) {
+				temp[eventsIridium[i]] = $(o).find("td").eq(i + 1).text();
+			}
+			temp["url"] = "https://www.heavens-above.com/" + $(o).find("td").eq(0).find("a").attr("href").replace("type=V", "type=A");
+			queue.push(temp);
+		});
+		function factory(temp) {
+			return new Promise((resolve, reject) => {
+				request(utils.iridium_options(temp["url"]), (error, response, body) => {
+					console.log(body)
+					if (error || response.statusCode !== 200) { //在无SessionID时返回500
+						reject(error);
+						return;
 					}
-				});
-			}, (errs, results) => {
-				if (errs) throw errs; // errs = [err1, err2, err3]
-				//console.log(results); // results = [result1, result2, result3]
-				database = database.concat(results);
-				$("form").find("input").each((i, o) => {
-					if ($(o).attr("name") === "ctl00$cph1$btnPrev" || $(o).attr("name") === "ctl00$cph1$visible") return;
-					else next += `&${$(o).attr("name")}=${$(o).attr("value")}`;
-				});
-				next += "&ctl00$cph1$visible=radioVisible";
-				next = next.replace(/\+/g, "%2B").replace(/\//g, "%2F") //.replace(/\$/g, "%24");
-				if (counter < config.count) {
-					getTable({
-						count: config.count,
-						root: config.root,
-						counter: ++counter,
-						opt: next,
-						database: database
+					var $ = cheerio.load(body, {
+						decodeEntities: false
 					});
-				} else {
-					fs.appendFile(basedir + "index.json", JSON.stringify(database), (err) => {
+					var table = $("form").find("table.standardTable"),
+						tr = table.find("tbody tr");
+					[
+						[6, 0],
+						[7, 1],
+						[8, 6],
+						[9, 7],
+						[10, 9],
+						[11, 10],
+						[12, 11]
+					].forEach((ele) => {
+						temp[eventsIridium[ele[0]]] = tr.eq(ele[1]).find("td").eq(1).text();
+					});
+					temp[eventsIridium[13]] = "https://www.heavens-above.com/" + $("#ctl00_cph1_imgSkyChart").attr("src") //.replace("size=800", "size=1600");,
+					var id = utils.md5(Math.random().toString()) //temp[eventsIridium[6]];
+					temp[eventsIridium[14]] = id;
+					fs.appendFile(basedir + id + ".html", table.html(), (err) => {
 						if (err) console.log(err);
-					});
-				}
+					}); //保存表格
+					request.get(utils.image_options(temp[eventsIridium[13]])).pipe(fs.createWriteStream(basedir + id + ".png", {
+						"flags": "a"
+					})).on("error", (err) => {
+						console.error(err);
+					}); //下载图片
+					resolve(temp);
+				});
 			});
 		}
+		Promise.allSettled(queue.map(temp => factory(temp))).then(results => {
+			results = results.filter(p => p.status === "fulfilled").map(p => p.value);
+			database = database.concat(results);
+			$("form").find("input").each((i, o) => {
+				if ($(o).attr("name") === "ctl00$cph1$btnPrev" || $(o).attr("name") === "ctl00$cph1$visible") return;
+				else next += `&${$(o).attr("name")}=${$(o).attr("value")}`;
+			});
+			next += "&ctl00$cph1$visible=radioVisible";
+			next = next.replace(/\+/g, "%2B").replace(/\//g, "%2F") //.replace(/\$/g, "%24");
+				if (counter++ < config.pages) {
+				getTable({
+					count: config.count,
+					pages: config.pages,
+					root: config.root,
+					counter,
+					opt: next,
+					database
+				});
+			} else {
+				fs.appendFile(basedir + "index.json", JSON.stringify(database), (err) => {
+					if (err) console.log(err);
+				});
+			}
+		});
 	});
 }
 
